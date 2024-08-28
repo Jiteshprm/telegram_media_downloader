bcf60ce8579e1b3560d175827477c474d748bb09
Jitesh Pramodray <jiteshprm@gmail.com>
2024-08-28 23:11:04 +0100

    -create folder conf to store api secrets outside git -load config.yaml by command line arg -create service and timer

--- media_downloader.py
+++ media_downloader.py
@@ -377,7 +377,8 @@ async def begin_import(config: dict, pagination_limit: int) -> dict:
             messages_list = []
             messages_list.append(message)
             # config["last_read_message_id"] = last_read_message_id
-            update_config(config)
+            config_file = sys.argv[1]
+            update_config(config_file, config)
     if messages_list:
         last_read_message_id = await process_messages(
             client,
