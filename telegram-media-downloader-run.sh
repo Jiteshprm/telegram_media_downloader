f29e20c1b000da0f92113af0e380ad623b88d781
Jitesh Pramodray <jiteshprm@gmail.com>
2024-08-28 22:44:31 +0100

    -create folder conf to store api secrets outside git -load config.yaml by command line arg -create service and timer

--- telegram-media-downloader-run.sh
+++ telegram-media-downloader-run.sh
@@ -3,7 +3,7 @@
 if [ ! -d conf ]; then
   echo "conf folder does not exist, creating and copy new config file"
   mkdir conf
-  cp config,yaml conf
+  cp config.yaml conf
 fi
 
 python3 media_downloader.py conf/config.yaml
